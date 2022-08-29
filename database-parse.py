a5881e6c7981e1c9d3b6d42976a41d237ed93f9c
remrama <mallett.remy@gmail.com>
2022-08-29 15:42:07 -0600

    incorporate pubmed into search and parse

--- database-parse.py
+++ database-parse.py
@@ -23,14 +23,34 @@ query = args.query
 
 import_dir = Path("./data")
 import_paths = list(import_dir.glob(f"db-{database}_query-*"))
-assert len(import_paths) == 1
-import_path = import_paths[0]
+if database == "crossref":
+    assert len(import_paths) == 1
+    import_path = import_paths[0]
+elif database == "pubmed":
+    assert len(import_paths) == 2
+    import_path, metadata_path = sorted(import_paths)
 
 with open(import_path, "r", encoding="utf-8") as fp:
     data = json.load(fp)
 
-    results_list =  data["message"]["items"]
-    df = pd.DataFrame.from_records(results_list, columns=["DOI", "title"])
-    df["title"] = df["title"].str[0]
-    ser = df.set_index("DOI").squeeze("columns")
+    if database == "crossref":
+        results_list =  data["message"]["items"]
+        df = pd.DataFrame.from_records(results_list, columns=["DOI", "title"])
+        df["title"] = df["title"].str[0]
+        ser = df.set_index("DOI").squeeze("columns")
 
+    elif database == "pubmed":
+        results_list =  data["PubmedArticle"]
+        records = []
+        for r in results_list:
+            article_info = {}
+            title = r["MedlineCitation"]["Article"]["ArticleTitle"]
+            abstract = r["MedlineCitation"]["Article"]["Abstract"]["AbstractText"]
+            article_ids = r["PubmedData"]["ArticleIdList"]
+            for id_ in article_ids:
+                if id_.startswith("10.") and id_.count("/") > 0:
+                    break
+            article_info = dict(doi=id_, title=title, abstract=abstract)
+            records.append(article_info)
+            
+        df = pd.DataFrame.from_records(records, index="doi")
--- database-search.py
+++ database-search.py
@@ -1,7 +1,7 @@
 """
 Search a given <database> for a given <search term>.
 
-Export file format depends on the database (e.g., json for CrossRef, xml for PubMed).
+Export file format is json.
 Export log file with metadata about the search.
 """
 import argparse
@@ -9,7 +9,9 @@ import json
 from pathlib import Path
 import time
 
+from Bio import Entrez
 from habanero import Crossref
+
 
 
 parser = argparse.ArgumentParser()
@@ -17,23 +19,21 @@ parser.add_argument("-d", "--database", type=str, default="crossref")
     # required=True, help="Choose which database to search.", choices=["crossref", "pubmed"])
 parser.add_argument("-q", "--query", type=str, default="lucid dreaming")
     # required=True, help="Choose the database search term.")
+parser.add_argument("-l", "--limit", type=int, default=10)
 args = parser.parse_args()
 
 database = args.database
 query = args.query
+limit = args.limit
 
 
 # Get a timestamp string of today to add to filename.
-today = time.strftime("%Y%M%d")
+today = time.strftime("%Y%m%d")
 
-extensions = {
-    "crossref": ".json",
-    "pubmed": ".xml",
-}
 export_dir = Path("./data")
-export_path = (export_dir / f"db-{database}_query-{query}_date-{today}").with_suffix(extensions[database])
-
-
+export_stem =  f"db-{database}_query-{query}_date-{today}"
+export_suffix = ".json"
+export_path = export_dir / (export_stem+export_suffix)
 
 
 if database == "crossref":
@@ -51,7 +51,7 @@ if database == "crossref":
         query=query,
         # filter={"has_full_text": True},
         # offset=None, # 1 to 10000, use if iterating beyond limit param?
-        limit=20, # 1000 max
+        limit=limit, # 1000 max
         # select=["DOI", "title"],
         # cursor=None, cursor_max=5000, progress_bar=False, # use cursor for DEEP PAGING
     )
@@ -59,3 +59,41 @@ if database == "crossref":
     # Export results as json file.
     with open(export_path, "w", encoding="utf-8") as fp:
         json.dump(results, fp, indent=4, ensure_ascii=True)
+
+
+elif database == "pubmed":
+
+    # ESummary (document summary downloads)
+
+    # Set personal API keys.
+    Entrez.email = "lenter360@gmail.com"
+
+    # Search database.
+    with Entrez.esearch(
+            db=database,
+            term=query,
+            retmax=limit,
+            retmode="xml",
+        ) as handle:
+        record = Entrez.read(handle)
+
+    # Get DOIs, titles, etc.
+    record_ids = record["IdList"]
+
+    with Entrez.efetch(
+        db=database,
+        id=record_ids,
+        retmode="xml",
+        #rettype="abstract",
+        ) as handle:
+        # results = handle.read()
+        results = Entrez.read(handle)
+
+    # Export full search results.
+    with open(export_path, "w", encoding="utf-8") as fp:
+        json.dump(results, fp, indent=4, ensure_ascii=True)
+
+    # Export record json, which has search parameters etc.
+    export_path_metadata = export_path.with_stem(export_stem+"_metadata")
+    with open(export_path_metadata, "w", encoding="utf-8") as fp:
+        json.dump(record, fp, indent=4, ensure_ascii=True)
